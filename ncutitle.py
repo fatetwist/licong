681ffad3055ec088c1e05dc44d5fe307d3d71df8
licong <591210216@qq.com>
2017-10-16 16:16:39 +0800

    first commit

---
+++ ncutitle.py
@@ -0,0 +1,14 @@
+import urllib.request
+import bs4
+from bs4 import BeautifulSoup
+
+url = 'http://news.ncu.edu.cn/'
+res = urllib.request.urlopen(url)
+soup = BeautifulSoup(res.read(),'html.parser',from_encoding='utf-8')
+node_title = soup.find('title')
+title = node_title.get_text()
+news = soup.select('.bknews')
+print(title)
+print('南大要闻：')
+for new in news:
+	print(new.get_text())
