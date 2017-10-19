e06bfb217922a1ee187ee064030dc055345f9860
licong <591210216@qq.com>
2017-10-20 00:27:07 +0800

    licong

--- ncutitle.py
+++ ncutitle.py
@@ -2,13 +2,15 @@ import urllib.request
 import bs4
 from bs4 import BeautifulSoup
 
-url = 'http://news.ncu.edu.cn/'
+# Web
+url='http://news.ncu.edu.cn/'
 res = urllib.request.urlopen(url)
 soup = BeautifulSoup(res.read(),'html.parser',from_encoding='utf-8')
+# 网页标题
 node_title = soup.find('title')
 title = node_title.get_text()
-news = soup.select('.bknews')
+# 其他标题
+title_others = soup.findAll('ul',{'class':{'newslist'}})
 print(title)
-print('南大要闻：')
-for new in news:
-	print('-',new.get_text())
+for y in title_others:
+    print(y.get_text())
